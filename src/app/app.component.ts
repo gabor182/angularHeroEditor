157aaaec6653e68eff99b6fbb5dbb94088f12d95
Gabor Schmied <schmied.gabor@gmail.com>
2017-07-30 13:16:39 +0200

    separation of concerns: AppComponent is only a navigation component, HeroesComponent created for the list of heroes, DashboardComponent dummy component created for dashboard view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,10 @@
-﻿import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
+﻿import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+    selector: 'my-app',
+    templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
-    title = 'Tour of Heroes';
-    selectedHero: Hero;
-    heroes: Hero[];
 
-    constructor(private heroService: HeroService) {
-        
-    }
-
-    ngOnInit(): void {
-        this.getHeroes();
-    }
-
-    getHeroes(): void {
-        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-    }
-
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
+export class AppComponent {
+    title: string = 'Tour of Heroes';
 }
---
+++ src/app/heroes.component.ts
@@ -0,0 +1,30 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+    title = 'Tour of Heroes';
+    selectedHero: Hero;
+    heroes: Hero[];
+
+    constructor(private heroService: HeroService) {
+        
+    }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+}
