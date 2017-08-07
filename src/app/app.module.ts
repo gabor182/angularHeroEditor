1c091e59d639ae56f2171d0972dfec8f089ba1f5
Gabor Schmied <gabor.schmied@semilab.hu>
2017-08-07 10:45:57 +0200

    more styling; added HttpModule to AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 ﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
@@ -21,6 +22,7 @@ import { HeroService } from './hero.service';
   imports: [
       BrowserModule,
       FormsModule,
+      HttpModule,
       AppRoutingModule
   ],
   providers: [HeroService],
